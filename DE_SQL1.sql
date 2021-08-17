f424b74c6385ef6e3a4796da935af196f04b7d35
novianazi <azinovian115@gmail.com>
2021-08-17 08:36:05 +0700

    Update DE_SQL1.sql
    userId    AverageDuration    
    -------------------------
    1         12

--- DE_SQL1.sql
+++ DE_SQL1.sql
@@ -5,5 +5,5 @@
   
   Write a query that selects userId and average session duration for each user who has more than one session.*/
   
-  -- Write only the SQL statement that solves the problem and nothing else.
-SELECT userId, AVG(duration) FROM sessionS GROUP BY userId HAVING COUNT(1) > 1;
+-- Write only the SQL statement that solves the problem and nothing else.
+SELECT userId, AVG(duration) AverageDuration FROM sessionS GROUP BY userId HAVING COUNT(1) > 1;
